7335534b0799779b89726996b1eac29ef83cb5c6
Tomas Sereikis <tomas.sereikis@gmail.com>
2019-01-28 17:02:06 +0200

    logger update

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,6 @@
 import * as pino from 'pino'
 
-const prettyPrintDevelopment = { translateTime: true, colorize: false }
+const prettyPrintDevelopment = { translateTime: true }
 const prettyPrint = process.env.NODE_ENV !== 'development' ? prettyPrintDevelopment : false
 
 export const logger = pino({ prettyPrint })
