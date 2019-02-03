4dd1472022b517bf8df61e561f7fa4263d15888f
Tomas Sereikis <tomas.sereikis@gmail.com>
2019-02-03 15:31:46 +0200

    small changes

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,7 @@
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
 export enum Env {
   HOST = 'HOST',
   PORT = 'PORT',
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
-import * as dotenv from 'dotenv'
 import { server, registry } from './server'
 import { registerControllers } from './controllers'
 import { errorLogger, logger } from './logger'
-
-dotenv.config()
-main().catch(onFail)
 
 process.on('uncaughtException', (error: Error) => {
   errorLogger.error(`UncaughtException ${error.stack}`)
@@ -21,7 +17,9 @@ async function main() {
   logger.info(`Server running at: ${server.info.uri}`)
 }
 
-async function onFail(err: Error) {
+async function onFailed(err: Error) {
   errorLogger.error(err)
   process.exit(1)
 }
+
+main().catch(onFailed)
